748cd92677ac411c3ac2efd4b27f26c500d3f3bd
Max Kvardakov <jimbomaxkvr@gmail.com>
2022-02-14 14:35:55 +0300

    Start project

---
+++ src/main/java/com/app/sell_it/SellItApplication.java
@@ -0,0 +1,13 @@
+package com.app.sell_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/sell_it/SellItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sell_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
